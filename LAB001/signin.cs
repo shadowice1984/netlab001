c65c63f800d746930e81bdd3ed1c5c434a79c28a
shadowice1984 <shadowice1984@DESKTOP-QDQ210N>
2021-07-21 12:38:12 +0900

    添加项目文件。

---
+++ LAB001/signin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB001
+{
+    public partial class signin : Form
+    {
+        public signin()
+        {
+            InitializeComponent();
+        }
+
+        private void signin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
